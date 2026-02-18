4f278ed718442ed949fccdb96ea5147e1da757b5
hannu64 <hannu.tomi@gmail.com>
2026-02-18 21:16:52 +0200

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -1,2 +1,9 @@
 const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://sekuchatback-production.up.railway.app'
 const socket = new WebSocket(`wss://${backendUrl.replace('https://', '')}/ws/${hash}`)
+
+ws.send(JSON.stringify({ nick: nickname, content: input }))
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data)
+  setMessages(prev => [...prev, data])
+}
