136ebddaa7d058342f06dcdc8b2466294606ad79
hannu64 <hannu.tomi@gmail.com>
2026-02-18 22:54:08 +0200

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -5,5 +5,14 @@ ws.send(JSON.stringify({ nick: nickname, content: input }))
 
 socket.onmessage = (event) => {
   const data = JSON.parse(event.data)
-  setMessages(prev => [...prev, data])
+  console.log('Received:', data)
+
+  // Avoid duplicate in sender's view (check nick)
+  setMessages(prev => {
+    // Only add if not already the last message (simple dedupe)
+    if (prev.length > 0 && prev[prev.length - 1].content === data.content && prev[prev.length - 1].nick === data.nick) {
+      return prev
+    }
+    return [...prev, data]
+  })
 }
